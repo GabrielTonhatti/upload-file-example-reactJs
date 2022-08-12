a905f7b4981321e3f2fda3787e312035972c55d9
Gabriel Tonhatti <gabrieltonhatti@xbrain.com.br>
2022-08-11 21:36:24 -0300

    Finalizando envio de imagens do frontend

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,27 +1,47 @@
-import React, { Component } from "react";
-import Dropzone from "react-dropzone";
+import React from "react";
+import { useDropzone } from "react-dropzone";
 
 import { DropContainer, UploadMessage } from "./styles";
 
-export default class Upload extends Component {
-    render() {
+const Upload = (props) => {
+    const { onUpload } = props;
+
+    const { getRootProps, getInputProps, isDragActive, isDragReject } =
+        useDropzone({
+            accept: {
+                "image/*": ["*"],
+            },
+            onDropAccepted: onUpload,
+        });
+
+    const renderDragMessage = (isDragActive, isDragReject) => {
+        if (!isDragActive) {
+            return <UploadMessage>Arraste arquivos aqui...</UploadMessage>;
+        }
+
+        if (isDragReject) {
+            return (
+                <UploadMessage type="error">
+                    Arquivo não suportado
+                </UploadMessage>
+            );
+        }
+
         return (
-            <Dropzone accept="image/*" onDropAccepted={() => {}}>
-                {({
-                    getRootProps,
-                    getInputProps,
-                    isDragActive,
-                    isDragReject,
-                }) => (
-                    <DropContainer
-                        {...getRootProps()}
-                        isDragActive={isDragActive}
-                        isDragReject={isDragReject}
-                    >
-                        <input {...getInputProps()} />
-                    </DropContainer>
-                )}
-            </Dropzone>
+            <UploadMessage type="success">Solte os arquivos aqui</UploadMessage>
         );
-    }
-}
+    };
+
+    return (
+        <DropContainer
+            {...getRootProps()}
+            isDragActive={isDragActive}
+            isDragReject={isDragReject}
+        >
+            <input {...getInputProps()} />
+            {renderDragMessage(isDragActive, isDragReject)}
+        </DropContainer>
+    );
+};
+
+export default Upload;
